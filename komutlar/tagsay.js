c8b6ec8c8d32f8f0d419ae135efbd4602bbb34fb
SehinsahPodosv2 <79246898+SehinsahPodosv2@users.noreply.github.com>
2021-02-18 10:29:54 +0300

    Add files via upload

---
+++ komutlar/tagsay.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require("discord.js");
+module.exports.run = (client, message, args) => {
+  
+//-------------------------------------------------------------------------------\\
+  
+if(!['809285603652010009'].some(role => message.member.roles.cache.get(role)) && !message.member.hasPermission('ADMINISTRATOR')) 
+return message.channel.send(new MessageEmbed().setDescription(`${message.author} Komutu kullanmak için yetkin bulunmamakta.`).setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+  
+//-------------------------------------------------------------------------------\\  
+  
+
+let tag1 = "Ψ";
+const ttag1 = message.guild.members.cache.filter(m => m.user.username.includes(tag1)).size
+
+const embed = new MessageEmbed()
+.setColor('RANDOM')
+message.channel.send(embed.setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setDescription(`Tagımızda **${ttag1}**`));
+};
+
+  exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["tag-say"],
+  permLvl: 0,
+}
+
+  exports.help = {
+  name: 'taglı'
+}
